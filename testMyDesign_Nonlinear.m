8c231028c84dbef3f955200cbe6ee79cab9d1d1c
Noe Guisset <noe.guisset@gmail.com>
2021-02-17 15:17:50 +0000

    fixed bug in constraint on load

--- testMyDesign_Nonlinear.m
+++ testMyDesign_Nonlinear.m
@@ -15,10 +15,10 @@ clear variables
 
 % The percentage perturbation to perform on each crane model parameter
 % The default is 10%
-perturbSize = 0.1;
+perturbSize = 0.2;
 
 % The part of the core coursework that is being worked on
-partNum = 3;
+partNum = 1;
 
 
 %% Load the parameters for the Simulation model
